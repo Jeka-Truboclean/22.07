58b10074222fce474adb23217266c5c92f3a151c
Eugen Karaman <karamanjekakol@gmail.com>
2024-07-18 17:21:44 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Win32;
+namespace _22._07
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string keyPath = @"Software\MyApplication";
+            DateTime lastAccessTime = LoadLastTimeVisit(keyPath);
+            if (lastAccessTime == DateTime.MinValue)
+            {
+                Console.WriteLine("Welcome to _22._07");
+            }
+            else
+            {
+                Console.WriteLine("Last access time: " + lastAccessTime);
+            }
+            SaveLastTimeVisit(keyPath, DateTime.Now);
+        }
+        static DateTime LoadLastTimeVisit(string keyPath)
+        {
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey(keyPath))
+            {
+                if (key != null)
+                {
+                    object value = key.GetValue("LastAccessTime");
+                    if (value != null)
+                    {
+                        return Convert.ToDateTime(value);
+                    }
+                }
+            }
+            return DateTime.MinValue;
+        }
+        static void SaveLastTimeVisit(string keyPath, DateTime lastAccessTime)
+        {
+            using (RegistryKey key = Registry.CurrentUser.CreateSubKey(keyPath))
+            {
+                key.SetValue("LastAccessTime", lastAccessTime.ToString());
+            }
+        }
+    }
+}
